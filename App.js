dee0c6724f79a9d44d5e231484ceade72414a98a
Karl Jonsson <karl.karbin@gmail.com>
2021-04-14 14:44:18 +0200

    API, flatlist, searchbar

--- App.js
+++ App.js
@@ -5,12 +5,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import OverViewPage from './components/OverviewPage';
 import {StatusBar} from 'react-native';
 
+global.data = [{id:"1"},{id:"2"},{id:"3"},]
+global.data2;
 
 const Tab = createMaterialTopTabNavigator();
 
 StatusBar.setBarStyle('light-content', true);
 
 export default function App() {
+  getData();
   return (
     <NavigationContainer>
       <Tab.Navigator
@@ -28,3 +31,22 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+const fetchCountries = async () => {
+  try {
+    let response = await fetch("https://coronavirus-19-api.herokuapp.com/countries");
+    let json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getData = async () => {
+  fetchCountries().then(
+    (countries) => {
+    data2 = countries;
+    // console.log(data2);
+    }
+  );
+};
