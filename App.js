442347cfe13073a0381a3aea7ad8693cc657f412
radovaniljic <63045013+radovaniljic@users.noreply.github.com>
2021-04-15 16:24:17 +0200

    implemented loading indicator during api call

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect}from "react";
-import { StatusBar, StyleSheet, Image } from "react-native";
+import { StatusBar, StyleSheet, Image, ActivityIndicator, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import CountryList from "./components/CountryList";
@@ -10,6 +10,7 @@ StatusBar.setBarStyle("light-content", true);
 
 const App = () => {
   const [APIData, setAPIData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(()=>{
     fetchCountries();
@@ -20,57 +21,69 @@ const App = () => {
       let response = await fetch("https://coronavirus-19-api.herokuapp.com/countries");
       let json = await response.json();
       setAPIData(json);
+      setIsLoading(false);
     } catch (error) {
       console.error(error);
     }
   };
 
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        tabBarOptions={{
-          tabStyle: { height: 100 },
-          style: styles.tabBar,
-          labelStyle: { color: "#fffaf0" },
-          indicatorStyle: { backgroundColor: "#00ce7c", height: 3 },
-          showIcon: true,
-          activeTintColor: "white",
-        }}
-      >
-        <Tab.Screen
-          name="Overview"
-          children={() => 
-            <OverviewPage countries={APIData}/>
-          }
-          options={{
-            tabBarIcon: () => (
-              <Image
-                source={require("./assets/icons/earth.png")}
-                style={styles.tabBarIcon}
-              />
-            ),
+  if (isLoading) {
+    return (<View style={styles.loadingScreen}>
+      <ActivityIndicator size="large" color="#00ce7c" />
+    </View>);
+  } else {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+          tabBarOptions={{
+            tabStyle: { height: 100 },
+            style: styles.tabBar,
+            labelStyle: { color: "#fffaf0" },
+            indicatorStyle: { backgroundColor: "#00ce7c", height: 3 },
+            showIcon: true,
+            activeTintColor: "white",
           }}
-        />
-        <Tab.Screen
-          name="Countries"
-          children={() => 
-            <CountryList countries={APIData}/>
-          }
-          options={{
-            tabBarIcon: () => (
-              <Image
-                source={require("./assets/icons/flag.png")}
-                style={styles.tabBarIcon}
-              />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+        >
+          <Tab.Screen
+            name="Overview"
+            children={() => 
+              <OverviewPage countries={APIData}/>
+            }
+            options={{
+              tabBarIcon: () => (
+                <Image
+                  source={require("./assets/icons/earth.png")}
+                  style={styles.tabBarIcon}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Countries"
+            children={() => 
+              <CountryList countries={APIData}/>
+            }
+            options={{
+              tabBarIcon: () => (
+                <Image
+                  source={require("./assets/icons/flag.png")}
+                  style={styles.tabBarIcon}
+                />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
+  loadingScreen: {
+    flex: 1,
+    backgroundColor: "#202124",
+    justifyContent: "center",
+  },
   tabBar: {
     backgroundColor: "#202124",
     paddingTop: 10,
