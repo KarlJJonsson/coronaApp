dc9507c1706b1ba06ee2e6fd74dfcf91ca791481
radovaniljic <63045013+radovaniljic@users.noreply.github.com>
2021-04-16 10:21:19 +0200

    created CountryDetailsScreen

---
+++ components/CountryDetailsScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View } from "react-native";
+
+import TotalCard from "./TotalCard";
+import DailyCard from "./DailyCard";
+import Separator from "./Separator";
+
+const CountryDetailsScreen = ({ navigation, route }) => {
+  return (
+    <View>
+      {/*       <TotalCard country={country} />
+      <Separator />
+      <DailyCard country={country} />
+      <Separator /> */}
+    </View>
+  );
+};
+
+export default CountryDetailsScreen;
