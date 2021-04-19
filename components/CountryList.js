a2e7967561d6b67bdf324578e53b427e4f21370c
Karl Jonsson <karl.karbin@gmail.com>
2021-04-19 18:04:58 +0200

    sorting

--- components/CountryList.js
+++ components/CountryList.js
@@ -34,37 +34,33 @@ const CountryList = (props) => {
 
   const [query, setQuery] = useState("");
   const [data, setData] = useState([]);
-  const [fullData, setFullData] = useState([]);
+  const [unSortedFullData, setUnSortedFullData] = useState([]);
+  const [sortedFullData, setSortedFullData] = useState([]);
   const [dropdownValue, setDropdown] = useState("Cases");
   const dropdownOptions = ["Cases", "Recovered", "Deaths", "Active"];
-  const [sortOrder, setSortOrder] = useState(1);
+  const [sortOrder, setSortOrder] = useState(true);
+  const [sortedData, setSortedData] = useState([]);
+  const [unSortedData, setUnSortedData] = useState([]);
 
   useEffect(() => {
     firstSort();
-    setFullData(
-      props.countries.filter(function (element) {
-        return element.country != "World";
-      })
-    );
   }, [props]);
 
   firstSort = () => {
     const unsortedInitData = props.countries.filter(function (element) {
       return element.country != "World";
     });
-    const initData = unsortedInitData.sort(((a, b) => (getAmount(a, dropdownValue) >= getAmount(b, dropdownValue)) ? (sortOrder*-1): sortOrder));
-    return setData(initData);
-  }
 
-  inverseSortOrder = () => {
-    if(sortOrder == 1){
-      setSortOrder(-1);
-    }
-    else if(sortOrder == -1){
-      setSortOrder(1);
-    }
-    const filteredData = data.sort(((a, b) => (getAmount(a, dropdownValue) >= getAmount(b, dropdownValue)) ? (sortOrder*-1): sortOrder));
-    setData(filteredData);
+    const unsortedInitData2 = props.countries.filter(function (element) {
+      return element.country != "World";
+    });
+    const initData = unsortedInitData.sort(((a, b) => (getAmount(a, dropdownValue) >= getAmount(b, dropdownValue)) ? -1: 1));
+    setSortedData(initData);
+    setSortedFullData(initData);
+    
+    const initData2 = unsortedInitData2.sort(((a, b) => (getAmount(a, dropdownValue) >= getAmount(b, dropdownValue)) ? 1: -1));
+    setUnSortedFullData(initData2);
+    setUnSortedData(initData2);
   }
 
   updateSearch = (search) => {
@@ -74,100 +70,121 @@ const CountryList = (props) => {
 
   updateDropdown = (filter) => {
     setDropdown(filter);
-    applyFilterParams(query, filter);
+    applyFilterParams(query);
   }
 
-  applyFilterParams = (search, filter) => {
+  applyFilterParams = (search) => {
     const formattedSearch = search.toLowerCase();
-    const searchedData = fullData.filter((country) => {
+    const searchedData1 = sortedFullData.filter((country) => {
       return contains(country.country.toLowerCase(), formattedSearch);
     });
-    const filteredData = searchedData.sort(((a, b) => (getAmount(a, filter) >= getAmount(b, filter)) ? (sortOrder*-1): sortOrder));
-    setData(filteredData);
+    const searchedData2 = unSortedFullData.filter((country) => {
+      return contains(country.country.toLowerCase(), formattedSearch);
+    });
+    setSortedData(searchedData1);
+    setUnSortedData(searchedData2);
   }
-  return (
-    <View style={styles.page}>
-      <SearchBar
-        round
-        onChangeText={(text) => updateSearch(text)}
-        containerStyle={{
-          backgroundColor: "#202124",
-          round: true,
-          borderBottomColor: "#202124",
-          borderTopColor: "#202124",
-        }}
-        inputContainerStyle={{
-          backgroundColor: "#202124",
-          borderWidth: 1,
-          borderColor: "#737373",
-          borderBottomColor: "#737373",
-          borderBottomWidth: 1,
-        }}
-        value={query}
-        placeholder="Sök land"
-        placeholderTextColor="#999999"
-      />
-       {/* DropdownMenu */}
-      <View style={styles.dropdown}>
-        <ModalDropdown
-          options={dropdownOptions}
-          textStyle={{ fontSize: 18, color: "#00ce7c" }}
-          dropdownStyle={{ backgroundColor: "#202124" }}
-          defaultValue={"Sorting by Total Cases"}
-          defaultIndex={0}
-          isFullWidth={true}
-          dropdownTextStyle={{ fontSize: 18 }}
-          onSelect={updateDropdown}
-        />
-        {/* SortOrder button */}
-        <TouchableOpacity onPress={() => {
-          inverseSortOrder();
-        }}>
-          <Ionicons name="swap-vertical" size={24} color="white" />
-        </TouchableOpacity>
-      </View>
 
-      <View style={styles.container}>
-        <FlatList
-          data={data}
-          renderItem={({ index, item }) => (
-            //Making entries touchable and sends user to CountryDetailsScreen
-            <TouchableWithoutFeedback
-              onPress={() =>
-                navigation.navigate("CountryDetails", {
-                  country: item,
-                  world: props.countries[0],
-                })
-              }
-            >
-              {/* rendering entry in list */}
-              <View style={styles.listing}>
-                <View style={styles.listingSubContainer}>
-                  {/* Adding index */}
-                  <Text style={styles.text}>{index + 1}.</Text>
-                  {/* Adding picture of country */}
-                  <Image
-                    style={{
-                      height: 25,
-                      width: 25,
-                      borderRadius: 25 / 2,
-                    }}
-                    source={gf.getImg(item.country.toLowerCase().replace(/\s/g, ''))}
-                  />
-                </View>
-                {/* Adding statistic to card, formatting statistic*/}
-                <View style={styles.listingSubContainer}>
-                  <Text style={styles.text}>
-                    {gf.numberWithCommas(getAmount(item, dropdownValue))}
-                  </Text>
-                </View>
-              </View>
-            </TouchableWithoutFeedback>
-          )}
-          keyExtractor={(item) => item.country}
+  return (
+    <>
+    {sortedData.length != 0 && (
+      <View style={styles.page}>
+        <SearchBar
+          round
+          onChangeText={(text) => updateSearch(text)}
+          containerStyle={{
+            backgroundColor: "#202124",
+            round: true,
+            borderBottomColor: "#202124",
+            borderTopColor: "#202124",
+          }}
+          inputContainerStyle={{
+            backgroundColor: "#202124",
+            borderWidth: 1,
+            borderColor: "#737373",
+            borderBottomColor: "#737373",
+            borderBottomWidth: 1,
+          }}
+          value={query}
+          placeholder="Sök land"
+          placeholderTextColor="#999999"
         />
+        {/* DropdownMenu */}
+        <View style={{
+          flexDirection: "row",
+          justifyContent: "space-between",}}
+        >
+          <View style={styles.dropdown}>
+            <ModalDropdown
+              options={dropdownOptions}
+              textStyle={{ fontSize: 18, color: "#00ce7c" }}
+              dropdownStyle={{ backgroundColor: "#202124" }}
+              defaultValue={"Sorting by Total Cases"}
+              defaultIndex={0}
+              isFullWidth={true}
+              dropdownTextStyle={{ fontSize: 18 }}
+              onSelect={updateDropdown}
+            />
+          </View>
+          <View style={{
+            justifyContent: "center",
+          }}>
+            {/* SortOrder button */}
+            <TouchableOpacity onPress={() => {
+              setSortOrder(!sortOrder);
+            }}
+            style={{
+              padding: 10
+            }}
+            >
+              <Ionicons name="swap-vertical" size={24} color="white" />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={styles.container}>
+          <FlatList
+            data={sortOrder === true ? sortedData : unSortedData}
+            renderItem={({ index, item }) => (
+              //Making entries touchable and sends user to CountryDetailsScreen
+              <TouchableWithoutFeedback
+                onPress={() =>
+                  navigation.navigate("CountryDetails", {
+                    country: item,
+                    world: props.countries[0],
+                  })
+                }
+              >
+                {/* rendering entry in list */}
+                <View style={styles.listing}>
+                  <View style={styles.listingSubContainer}>
+                    {/* Adding index */}
+                    <Text style={styles.text}>{index + 1}.</Text>
+                    {/* Adding picture of country */}
+                    <Image
+                      style={{
+                        height: 25,
+                        width: 25,
+                        borderRadius: 25 / 2,
+                      }}
+                      source={gf.getImg(item.country.toLowerCase().replace(/\s/g, ''))}
+                    />
+                  </View>
+                  {/* Adding statistic to card, formatting statistic*/}
+                  <View style={styles.listingSubContainer}>
+                    <Text style={styles.text}>
+                      {gf.numberWithCommas(getAmount(item, dropdownValue))}
+                    </Text>
+                  </View>
+                </View>
+              </TouchableWithoutFeedback>
+            )}
+            keyExtractor={(item) => item.country}
+          />
+        </View>
       </View>
-    </View>
+    )}
+  </>
   );
 };
 
@@ -178,8 +195,6 @@ const styles = StyleSheet.create({
   },
   dropdown: {
     padding: 10,
-    flexDirection: "row",
-    justifyContent: "space-between",
   },
   listing: {
     flexDirection: "row",
